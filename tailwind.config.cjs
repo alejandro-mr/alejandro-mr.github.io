9da0b5dd7f5d3de93d668a0131c2b37166e9fccd
Alejandro Moreno <jose.alejandro.mr@gmail.com>
2023-05-30 00:36:52 -0700

    Updating landing page with new ExperienceCard component.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -18,6 +18,19 @@ module.exports = {
       screens: {
         '2xl': '1680px',
       },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+        menu: {
+          '0%': {},
+        },
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+        menu: 'menu 2s ease-in-out infinite',
+      },
     },
   },
   plugins: [],
