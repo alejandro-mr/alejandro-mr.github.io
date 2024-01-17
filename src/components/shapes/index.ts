a34db82a2a4276b6d1a67ae86d4856c231dc7312
Alejandro Moreno <jose.alejandro.mr@gmail.com>
2024-01-17 02:10:40 -0700

    Removing old version of webpage.

--- src/components/shapes/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Circle } from "./circle.astro";
-export { default as Square } from "./square.astro";
-export { default as Triangle } from "./triangle.astro";
-export { default as Hexagon } from "./hexagon.astro";
