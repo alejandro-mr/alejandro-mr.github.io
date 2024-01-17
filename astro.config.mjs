1eff2e6b4df50b1201ec9175c8a79d02194487b5
Alejandro Moreno <jose.alejandro.mr@gmail.com>
2024-01-17 02:33:14 -0700

    Updating astro github actions deploy workflow.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,21 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
-import preact from "@astrojs/preact";
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://alejandro-mr.github.io',
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es'],
     fallback: {
-      es: 'en'
+      es: 'en',
     },
     routing: {
-      prefixDefaultLocale: false
-    }
+      prefixDefaultLocale: false,
+    },
   },
-  integrations: [tailwind(), mdx(), preact()]
+  integrations: [tailwind(), mdx(), preact()],
 });
+
