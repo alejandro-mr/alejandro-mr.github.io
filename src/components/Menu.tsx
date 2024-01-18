1e7d9d43548de2546125bbdd9cb0cdc8ed9cb215
Alejandro Moreno <jose.alejandro.mr@gmail.com>
2024-01-17 22:48:23 -0700

    Updating page styling to include different colors.

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -7,7 +7,7 @@ const hamburgerStyle = {
 function Menu() {
   const [open, setOpen] = useState(false);
   return (
-    <div className="select-none grid items-center justify-self-end h-6">
+    <div className="select-none grid items-center justify-self-end h-6 lg:text-lg">
       <button
         className="lg:hidden grid h-3 gap-y-1 w-6"
         onClick={() => setOpen((open) => !open)}
@@ -17,26 +17,26 @@ function Menu() {
         <div className={hamburgerStyle.common}></div>
       </button>
       <div
-        className={`bg-slate-50 drop-shadow-[0_-2px_3px_rgba(150,79,76,0.2)] text-sun-dried-tomato text-center font-semibold rounded-t-lg mx-1 fixed bottom-0 left-0 right-0 z-10 h-0 transition-all ${!open ? 'h-0 pt-0' : 'h-1/3'}`}
+        className={`opacity-95 max-lg:bg-slate-50 max-lg:drop-shadow-[0_-2px_3px_rgba(150,79,76,0.2)] max-lg:text-sun-dried-tomato text-center font-semibold rounded-t-lg mx-1 max-lg:fixed bottom-0 left-0 right-0 z-10 transition-all ${!open ? 'max-lg:h-0' : 'h-3/6 md:h-5/6'}`}
       >
         <div
-          className={`select-none cursor-pointer relative grid content-center -top-5 mx-auto rounded-full bg-gradient-to-b to-slate-50 via-pristine from-peach-puree drop-shadow-[0_2px_3px_rgba(150,79,76,0.2)] h-10 w-10 text-xl text-sun-dried-tomato ${!open && 'hidden'}`}
+          className={`select-none cursor-pointer lg:hidden relative grid content-center -top-5 mx-auto rounded-full bg-gradient-to-b to-slate-50 via-pristine from-peach-puree drop-shadow-[0_2px_3px_rgba(150,79,76,0.2)] h-10 w-10 text-xl text-sun-dried-tomato ${!open && 'hidden'}`}
           onClick={() => setOpen((open) => !open)}
         >
           &#x2715;
         </div>
-        <ul>
-          <li className="py-4">
+        <ul className="grid gap-2 lg:grid-cols-3 lg:gap-10 lg:text-pristine">
+          <li className="max-lg:py-4">
             <a href="/#hero" onClick={() => setOpen((open) => !open)}>
               About me
             </a>
           </li>
-          <li className="py-4">
+          <li className="max-lg:py-4">
             <a href="/blog" onClick={() => setOpen((open) => !open)}>
               Blog
             </a>
           </li>
-          <li className="py-4">
+          <li className="max-lg:py-4">
             <a href="#contact" onClick={() => setOpen((open) => !open)}>
               Contact
             </a>
